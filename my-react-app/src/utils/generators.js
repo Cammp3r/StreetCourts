bbe620897f2467e4b2c0cd496fd73388e81d9c13
Merqn <makitra.m.v.-im55@edu.kpi.ua>
2026-02-15 16:25:21 +0200

    color generator

--- my-react-app/src/utils/generators.js
+++ my-react-app/src/utils/generators.js
@@ -1,22 +1,37 @@
 // для стрічки імен, прийматиме реальний масив з бд пізніше
 
-
+// універсальний генератор, який циклічно перебирає будь-який масив (нескінченний генератор)
 export function* arrayCycler(array, step = 1) {
- let i = 0;
+  let i = 0;
   while (true) {
     yield array[i % array.length];
     i += step;
   }
 }
 
-// зробити генератор кольорів для рамки
+// генератор кольорів для рамки (приклад використання нескінченного генератора)
+export function* colorCycle(colors = ["red", "green", "blue"]) {
+  let i = 0;
+  while (true) {
+    yield colors[i % colors.length];
+    i += 1;
+  }
+}
 
-// тайм аут функція двигун
+// iterator   - будь-який ітератор / генератор
+// callback   - що робити з кожним значенням
+// interval   - як часто викликати next() (мс)
+// timeoutSec - скільки секунд працювати (опціонально).
+// Якщо timeoutSec не передано, двигун працює доки його явно не зупинять.
 export function runEngine(iterator, callback, interval) {
   const timer = setInterval(() => {
     const next = iterator.next();
+    if (next.done) {
+      clearInterval(timer);
+      return;
+    }
     callback(next.value);
   }, interval);
-  
+
   return () => clearInterval(timer);
 }
