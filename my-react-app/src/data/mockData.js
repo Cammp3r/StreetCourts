16a017c3bf0803c65f5d152bf22e70b2f3297fdb
larisacemeris955-boop <larisacemeris955@gmail.com>
2026-03-01 16:49:51 +0200

    denis task 1,2

--- my-react-app/src/data/mockData.js
+++ my-react-app/src/data/mockData.js
@@ -1,5 +1,48 @@
 export const REAL_DB_USERS = ['Макс', 'Олег', 'Андрій', 'Саша'];
 
+export const FRIENDS = [
+  {
+    id: 'oleg-1',
+    name: 'Олег',
+    handle: '@oleg_hoop',
+    initials: 'ОМ',
+    status: 'Online • Грає зараз',
+    sport: '🏀',
+  },
+  {
+    id: 'andrii-2',
+    name: 'Андрій',
+    handle: '@andrii_sport',
+    initials: 'АП',
+    status: 'Online • На майданчику',
+    sport: '⚽',
+  },
+  {
+    id: 'sasha-3',
+    name: 'Саша',
+    handle: '@sasha_ball',
+    initials: 'СК',
+    status: 'Було 2 години тому',
+    sport: '🏀',
+  },
+  {
+    id: 'kolya-4',
+    name: 'Коля',
+    handle: '@kolya_court',
+    initials: 'КВ',
+    status: 'Offline',
+    sport: '⚽',
+  },
+  {
+    id: 'dima-5',
+    name: 'Діма',
+    handle: '@dima_game',
+    initials: 'ДА',
+    status: 'Online • Шукає гру',
+    sport: '🏀',
+  },
+];
+
 export const COURTS = [
   {
     id: 'kpi',
