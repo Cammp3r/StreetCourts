45fad04450db0a1cfd572d60e1f4b76f0d119fd4
Merqn <makitra.m.v.-im55@edu.kpi.ua>
2026-05-17 12:00:00 +0300

    levels

--- my-react-app/src/utils/log.js
+++ my-react-app/src/utils/log.js
@@ -1,25 +1,55 @@
-function simpleLog(fn) {
-  if (typeof fn !== 'function') return fn;
-  return function (...args) {
-    try {
-      console.log('start', fn.name || 'anon');
-      const res = fn.apply(this, args);
-      if (res && typeof res.then === 'function') {
-        return res.then((r) => {
-          console.log('end', fn.name || 'anon');
-          return r;
-        }, (e) => {
-          console.error('error', e && e.message ? e.message : e);
-          throw e;
-        });
-      }
-      console.log('end', fn.name || 'anon');
-      return res;
-    } catch (e) {
-      console.error('error', e && e.message ? e.message : e);
-      throw e;
-    }
-  };
+const LEVELS = { DEBUG: 10, INFO: 20, ERROR: 40 };
+
+function normalize(level) {
+  if (!level) return LEVELS.INFO;
+  const up = String(level).toUpperCase();
+  return LEVELS[up] || LEVELS.INFO;
 }
 
-export default simpleLog;
+function now() {
+  return new Date().toISOString();
+}
+
+function createLogger(options = {}) {
+  const cfg = {
+    level: options.level || 'INFO',
+    includeArgs: options.includeArgs ?? true,
+    includeResult: options.includeResult ?? true,
+  };
+
+  function wrap(fn) {
+    if (typeof fn !== 'function') return fn;
+
+    return function (...args) {
+      const start = Date.now();
+      const label = fn.name || 'anonymous';
+      const level = cfg.level;
+      if (cfg.includeArgs) console.info(`[${now()}] [${level}] [${label}] args=${JSON.stringify(args)}`);
+      try {
+        const res = fn.apply(this, args);
+        if (res && typeof res.then === 'function') {
+          return res.then((r) => {
+            const dur = Date.now() - start;
+            if (cfg.includeResult) console.info(`[${now()}] [${level}] [${label}] result=${JSON.stringify(r)} duration=${dur}ms`);
+            return r;
+          }, (e) => {
+            const dur = Date.now() - start;
+            console.error(`[${now()}] [ERROR] [${label}] error=${e && e.message ? e.message : e} duration=${dur}ms`);
+            throw e;
+          });
+        }
+        const dur = Date.now() - start;
+        if (cfg.includeResult) console.info(`[${now()}] [${level}] [${label}] result=${JSON.stringify(res)} duration=${dur}ms`);
+        return res;
+      } catch (e) {
+        const dur = Date.now() - start;
+        console.error(`[${now()}] [ERROR] [${label}] error=${e && e.message ? e.message : e} duration=${dur}ms`);
+        throw e;
+      }
+    };
+  }
+
+  return { wrap };
+}
+
+export default createLogger;
